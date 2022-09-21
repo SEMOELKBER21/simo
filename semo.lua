8ca208ae2fc25fc158a468f3817b48a6ad6b6be2
SEMOELKBER21 <93216241+SEMOELKBER21@users.noreply.github.com>
2022-09-21 14:21:29 +0200

    Update semo.lua

--- semo.lua
+++ semo.lua
@@ -1028,7 +1028,7 @@ end
 end
 
 if text == 'السورس' or text == 'سورس' or text == 'يا سورس' or text == 'source' then
-Pphoto = "https://t.me/WORLED_SEMO/160"
+photo = "https://t.me/WORLED_SEMO/160"
 local T =[[
 The besT sourCe on TeLeGrAm
 ]]
